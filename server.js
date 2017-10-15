8cfb38ccc4eb6e862066870180a620e58fdfc12a
Anderson <asipe@redventures.com>
2017-10-14 21:08:42 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,93 @@
+var express = require('express')
+var bodyParser = require('body-parser');
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+    host     : 'localhost',
+    socketPath: '/Applications/MAMP/tmp/mysql/mysql.sock',
+    user     : 'ita_finalproject',
+    password : '4c4WSSTtEvZJl5xu',
+    database : 'ita_finalproject'
+  });
+
+var app = express();
+app.use(bodyParser.json());
+
+app.get('/',(req,res) => {
+    res.send('Hello World');
+})
+
+app.get('/products',(req,res) => {
+    //TODO: update response data to include a promise
+    var sqlString = 'SELECT * FROM products'
+    connection.query(sqlString,(error,results,fields) => {
+            if(error) {
+                res.send(error);
+            } 
+            res.send(results);
+        })
+})
+
+app.get('/orders',(req,res) => {
+    //TODO: update response data to include a promise
+    var sqlString = 'SELECT * FROM orders'
+    connection.query(sqlString,(error,results,fields) => {
+            if(error) {
+                res.send(error);
+            } 
+            res.send(results);
+        })
+})
+
+app.get('/orders/:id',(req,res) => {
+    //TODO: update response data to include a promise
+    var sqlString = 'SELECT * FROM orders_details WHERE OrderID = ?;'
+    connection.query(sqlString,[req.params.id],(error,results,fields) => {
+            if(error) {
+                res.send(error);
+            } 
+            res.send(results);
+        })
+})
+
+app.post('/orders',(req,res) => {
+    
+    //TODO: replace sql variable names
+    //TODO: update response data to include a promise
+    var sqlString = "INSERT INTO `orders` (`OrderTotal`, `CustomerID`) VALUES (?, ?);";
+    connection.query(sqlString,[req.body.total,req.body.customer],(error,results,fields) => {
+        if(error) {
+            res.send(error);
+        } else{
+            var orderID = results.insertId;
+            var sqlString3 = "INSERT INTO `orders_details` (`OrderID`, `ProductID`, `Quantity`, `Price`) VALUES ?"
+            connection.query(sqlString3,[
+                Array.from(req.body.items).map((item) => {
+                    
+                    return [orderID,...item]
+                })
+            ],(error,results,fields) => {
+                if(error){
+                    res.send(error)
+                }
+                res.send(results);
+            })
+        }
+        
+    })
+})
+
+app.delete('/orders/:id',(req,res) => {
+    //TODO: update response data to include a promise
+    var sql = "DELETE FROM `orders` WHERE `orders`.`OrderID` = ?;"
+    connection.query(sql,[req.params.id],(error,results,fields) => {
+        if(error){
+            res.send(error);
+        }
+        res.send(results);
+    })
+})
+
+app.listen(3000, () => {
+    console.log('Server is up.')
+});
